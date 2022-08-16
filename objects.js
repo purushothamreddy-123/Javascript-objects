eaa9159611d69190e50b3cb9a80a6c888ff9eda9
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-16 20:46:51 +0530

    Javascript objects

---
+++ objects.js
@@ -0,0 +1,17 @@
+// // using dot notation
+// var person = { 
+//     name: 'purushotham', 
+//     age: 22, 
+// };
+
+// console.log(person.name);
+// console.log(person.age);
+
+// using bracket notation
+var person = { 
+    name: 'purushotham', 
+    age: 22, 
+};
+
+console.log(person["name"]);
+console.log(person["age"]);
